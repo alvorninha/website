d503364191a72fd06549db9878e02f3ad3f69adb
alvorninha <66682526+alvorninha@users.noreply.github.com>
2021-04-15 16:43:57 +0200

    first commit

---
+++ js/login2.js
@@ -0,0 +1,24 @@
+var angemeldet = false;
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "admin" && password == "test") {
+        alert("Login als Administrator")
+        angemeldet = true
+        return false;
+
+    }
+    else {
+        alert("Login fehlgeschlagen");
+    }
+}
+
+
+
+
+
+
+
+
+
+
